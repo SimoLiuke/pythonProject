7327c486a14c16eec5d2e72a5dd81d227b5bebad
Simo <simo.liuke@gmail.com>
2022-08-25 11:51:15 +0300

    1.1

--- Exercises.py
+++ Exercises.py
@@ -1,7 +1,16 @@
-import sys
 
-sys
+import sys
+import math
+r = float(input("Give a radius of a circle: "))
+area = math.pi *r*r
+print("The are of a circle with that radius is = %.2f" %area)
+
+sys(exit(1))
+
+# 2.1
 user = input("Enter your name:\n ")
 
 print("Hello, " + user + "!")
-sys(exit(1))
+
+# 1.1
+print("Hello, Simo!")
