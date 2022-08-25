a687fece818c3cc81a8f0d49999250c024fff4a7
Simo <simo.liuke@gmail.com>
2022-08-25 11:56:14 +0300

    1.1

--- Exercises.py
+++ Exercises.py
@@ -1,11 +1,20 @@
-
-import sys
 import math
+import sys
+
+length = float(input('Please Enter the Length of a Rectangle: '))
+width = float(input('Please Enter the Width of a Rectangle: '))
+
+perimeter = 2 * (length + width)
+
+print("Perimeter of a Rectangle using", length, "and", width, " = ", perimeter)
+
+sys(exit(1))
+
+# 2.2
+
 r = float(input("Give a radius of a circle: "))
 area = math.pi *r*r
 print("The are of a circle with that radius is = %.2f" %area)
-
-sys(exit(1))
 
 # 2.1
 user = input("Enter your name:\n ")
