d13741634b8b90427c75482add468acd8a106d1a
Simo <simo.liuke@gmail.com>
2022-08-25 13:10:51 +0300

    1.2

--- Exercises.py
+++ Exercises.py
@@ -1,14 +1,29 @@
 import math
 import sys
 
+num1 = (int(input("Give any number between 0 - 9:\n")))
+num2 = (int(input("Give any number between 0 - 9:\n")))
+num3 = (int(input("Give any number between 0 - 9:\n")))
+
+sum_1 = num1 + num2 + num3
+prod_1 = num1*num2*num3
+avg_1 = (num1+num2+num3)/3
+print("The sum of the numbers is: ")
+print(sum_1)
+print("The product of the numbers is: ")
+print(prod_1)
+print("The average of the numbers is: ")
+print(avg_1)
+
+sys(exit(1))
+
+# 2.3
 length = float(input('Please Enter the Length of a Rectangle: '))
 width = float(input('Please Enter the Width of a Rectangle: '))
 
 perimeter = 2 * (length + width)
 
 print("Perimeter of a Rectangle using", length, "and", width, " = ", perimeter)
-
-sys(exit(1))
 
 # 2.2
 
